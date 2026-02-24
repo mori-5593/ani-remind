ef86d3facaa5c7009107c915e82b52a35d4a3a15
mori-5593 <brook2915@icloud.com>
2026-02-24 14:47:53 +0900

    testフォルダの語尾に.bakを記述し、一時的にテスト対象から外す

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :session, controller: "sessions"
+  resources :sessions
   resources :passwords, param: :token
 
   resources :users, only: [:new, :create]
