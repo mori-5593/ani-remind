0c824bf8f32eeb867239e9e6d7baffb5b1ecc97f
mori-5593 <brook2915@icloud.com>
2026-02-27 17:18:51 +0900

    投稿一覧のコントローラを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,14 @@
 class PostsController < ApplicationController
   included Authentication
 
+  skip_before_action :require_authentication, only: [:index, :show] #投稿一覧画面は誰でも見れるようにする
+
   def current_user
     Current.user
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
