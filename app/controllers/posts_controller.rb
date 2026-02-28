164a7e971ea3f5a2b34394027b919a291922a95b
mori-5593 <brook2915@icloud.com>
2026-02-28 17:47:48 +0900

    postsコントローラーの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :require_authentication, only: [:index] #投稿一覧画面は誰でも見れるようにする
 
-  def current_user
-    Current.user
-  end
-
   def index
     @posts = Post.all.order(created_at: :desc)
   end
