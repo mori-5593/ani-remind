33265163c85cc1f7deb474d07a8791b1c1dd2e46
mori-5593 <brook2915@icloud.com>
2026-03-03 17:57:46 +0900

    検索機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
   before_action :ensure_current_user, only: [ :edit, :update, :destroy ]
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: :true).order(created_at: :desc)
     @page_title = "投稿一覧"
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   validates :rating, presence: true, inclusion: { in: 1..5 }, if: -> { watched? }
   enum :status, { watched: 0, want_to_watch: 1 } # watched: みた, wat_to_watch: みたい
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["annict_id", "content", "created_at", "id", "image_url", "rating", "status", "title", "updated_at", "user_id"]
+  end
+
   def status_i18n
     case status
     when "watched" then "みた"
