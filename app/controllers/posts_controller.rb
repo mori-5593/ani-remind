ca076e0515af9214aaa28988076d290dde21b2ad
mori-5593 <brook2915@icloud.com>
2026-03-03 09:40:13 +0900

    コントローラーの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿を更新しました"
+    else
+      render :edit, status: unprocessable_entity
+    end
+  end
+
   def watched
     # ステータスが「見た（０）」の投稿だけ取得
     @posts = current_user.posts.watched
