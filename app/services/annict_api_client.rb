32891f89753c0e4addc51d890905e07de5038fc0
mori-5593 <brook2915@icloud.com>
2026-03-03 17:10:01 +0900

    lintチェック修正

--- app/services/annict_api_client.rb
+++ app/services/annict_api_client.rb
@@ -1,14 +1,14 @@
 class AnnictApiClient
-  BASE_URL = 'https://api.annict.com'
+  BASE_URL = "https://api.annict.com"
 
   def initialize
     @conn = Faraday.new(url: BASE_URL)
   end
 
   def search_works(title)
-    response = @conn.get('/v1/works') do |req|
-      req.params['filter_title'] = title
-      req.params['access_token'] = ENV['ANNICT_ACCESS_TOKEN']
+    response = @conn.get("/v1/works") do |req|
+      req.params["filter_title"] = title
+      req.params["access_token"] = ENV["ANNICT_ACCESS_TOKEN"]
     end
 
     return [] unless response.status == 200
