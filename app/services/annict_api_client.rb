93e7292fc5b6afc60618734ea3751860073a5f72
mori-5593 <brook2915@icloud.com>
2026-03-03 13:55:45 +0900

    service objectを使ってAPIを呼び出す処理を実装

---
+++ app/services/annict_api_client.rb
@@ -0,0 +1,18 @@
+class AnnictApiClient
+  BASE_URL = 'https//api.annict.com'
+
+  def initialize
+    @conn = Faraday.new(url: BASE_URL)
+  end
+
+  def search_works(title)
+    response = @conn.get('/v1/works') do |req|
+      req.params['filter_title'] = title
+      req.params['access_token'] = ENV['ANNICT_ACCESS_TOKEN']
+    end
+
+    return [] unless response.status == 200
+
+    JSON.parse(response.body)["works"]
+  end
+end
