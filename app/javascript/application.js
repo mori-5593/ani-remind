fd9f720f38e077929608fa6530ccf179306907fa
mori-5593 <brook2915@icloud.com>
2026-03-09 18:57:20 +0900

    ヘッダーをハンバーガーメニューにレスポンシブ化

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("menu-button")
+  const menu = document.getElementById("mobile-menu")
+
+  if (button) {
+    button.addEventListener("click", () => {
+      menu.classList.toggle("hidden")
+    })
+  }
+})
