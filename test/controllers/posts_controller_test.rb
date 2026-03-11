9f788d63f90896bb1436eec179bcfd02dcdc616e
mori-5593 <brook2915@icloud.com>
2026-03-11 16:11:09 +0900

    テストコードエラー修正

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -11,11 +11,19 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get create" do
+  test "should get create and redirect to posts index by default" do
     assert_difference("Post.count") do
       post posts_url, params: { post: { title: "Test Title", rating: 3, status: "watched", content: "test" } }
     end
 
-    assert_redirected_to post_url(Post.last) # 投稿後のリダイレクト先
+    assert_redirected_to posts_url # デフォルトは投稿一覧画面
+  end
+
+  test "should get create and redirect to mypage when from is mypage" do
+    assert_difference("Post.count") do
+      post posts_url, params: { post: { title: "Mypage Post", rating: 3, status: "watched", content: "test", from: "mypage" } }
+    end
+
+    assert_redirected_to user_url(@user) # マイページへの遷移を確認
   end
 end
