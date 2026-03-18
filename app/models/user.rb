5e94cfb248ef7c76fe51e42fc72109c1c9ac163c
mori-5593 <brook2915@icloud.com>
2026-03-18 17:18:28 +0900

    プロフィール編集時のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   validates :email_address, presence: true, uniqueness: true
+  # allow_nilはpassword属性がnilの場合バリデーションエラーが発生しない、
+  # 空文字の場合適応されずpresence: trueによるバリデーションエラーが発生する
+  # 値がある場合presence: trueによるバリデーションエラーが発生しない
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
