5c4d3529d0d7c0df9e321d77153c2ba27c8df479
mori-5593 <brook2915@icloud.com>
2026-03-25 15:57:13 +0900

    lintチェックとコントローラテストの作成

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,13 +1,31 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get comments_create_url
-    assert_response :success
+  setup do
+    @post = posts(:one) # コメントがつく記事
+    @user = users(:one) # コメント投稿者
+    sign_in_as(@user) # ログイン状態にする
   end
 
-  test "should get destroy" do
-    get comments_destroy_url
-    assert_response :success
+  # コメント投稿のテスト
+  test "should create comment" do
+    @user = users(:one) # テスト用のユーザー準備
+
+    assert_difference("Comment.count", 1) do
+      post post_comments_url(@post), params: { comment: { content: "テストコメント" } }
+    end
+    assert_redirected_to post_url(@post)
+  end
+
+  # コメント削除のテスト
+  test "should destroy comment" do
+    @comment = comments(:one) # 消したいコメントを取得
+    @post = @comment.post # そのコメントが紐づいている記事を取得
+
+    assert_difference("Comment.count", -1) do
+      # ルーティングに合わせて@post, @commentの両方を引数に入れる
+      delete post_comment_url(@post, @comment)
+    end
+    assert_redirected_to post_url(@post)
   end
 end
